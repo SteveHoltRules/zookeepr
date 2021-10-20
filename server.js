07b369f286167ed96d8d25a3491db77a4c93041f
Matt Luna <mat.c.luna@gmail.com>
2021-10-20 13:31:12 -0500

    front end working

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+//static files
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 
@@ -120,12 +122,27 @@ function validateAnimal(animal) {
   if(!animal.diet || typeof animal.diet !== 'string') {
     return false;
   }
-
   if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
     return false;
   }
   return true;
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
